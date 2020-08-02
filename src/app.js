698b6341316ebf5e7619a4e0c45558008b6dfb7d
Pandurang Kamath <pandu1990@gmail.com>
2020-08-02 16:37:29 -0700

    Setting up react-router with some example pages.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const rootAppElement = document.getElementById('app');
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
 
-ReactDOM.render(<p>This is my boilerplate</p>, rootAppElement);
+const AddExpensePage = () => (
+  <div>
+    This is my add expense page
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is my edit expense page
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is my help page
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
