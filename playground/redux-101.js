7259850fb141cdcf2bb5b54c55712a544599b7d7
Pandurang Kamath <pandu1990@gmail.com>
2020-08-07 23:44:18 -0700

    redux-101.js Refactor Action Generators as functions

--- playground/redux-101.js
+++ playground/redux-101.js
@@ -1,14 +1,33 @@
 import { createStore } from 'redux';
 
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
-        count: state.count + (typeof action.incrementBy === 'number' ? action.incrementBy : 1)
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
       return {
-        count: state.count - (typeof action.decrementBy === 'number' ? action.decrementBy : 1)
+        count: state.count - action.decrementBy
       };
     case 'RESET':
       return {
@@ -27,29 +46,14 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: 'DECREMENT'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(setCount({ count: 101 }));
