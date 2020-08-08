833b3bcaa3488b505060ee41b03f4a5ec454cdac
Pandurang Kamath <pandu1990@gmail.com>
2020-08-08 00:43:39 -0700

    redux-expensify.js Adding action generators for expense reducer.

--- playground/redux-expensify.js
+++ playground/redux-expensify.js
@@ -1,12 +1,65 @@
 import { createStore, combineReducers } from 'redux';
+import { v4 as uuidv4 } from 'uuid';
+
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuidv4(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+const removeExpense = (id) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
 
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
 };
+
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
 
 const filtersReducerDefaultState = {
   text: '',
@@ -16,6 +69,11 @@ const filtersReducerDefaultState = {
 };
 const filtersReducers = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
     default:
       return state;
   }
@@ -26,4 +84,15 @@ const store = createStore(combineReducers({
   filters: filtersReducers
 }));
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const { expense: { id: rentId } } = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const { expense: { id: coffeeId } } = store.dispatch(addExpense({ description: 'Coffee', amount: 3 }));
+
+store.dispatch(removeExpense(rentId));
+store.dispatch(editExpense(coffeeId, { amount: 5 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
