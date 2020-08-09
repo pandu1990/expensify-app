891dd03adcefdfc815a41bb7be21d7c7e87799dd
Pandurang Kamath <pandu1990@gmail.com>
2020-08-08 22:52:06 -0700

    Configuring Redux DevTools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,10 @@ import { createStore, combineReducers } from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducers from '../reducers/filters';
 
-export default () => createStore(combineReducers({
-  expenses: expensesReducer,
-  filters: filtersReducers
-}));
+export default () => createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducers
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
