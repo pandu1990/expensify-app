14ef6c4f85ded277e2ea22283a138c20f768ef1d
Pandurang Kamath <pandu1990@gmail.com>
2021-06-22 01:14:51 -0700

    Wiring up add and edit expense form and dispatching events to udpate the redux store

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
