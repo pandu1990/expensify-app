941eb0e322b7d6d08afe28b04de2750255b57d5c
Pandurang Kamath <pandu1990@gmail.com>
2021-06-21 23:18:34 -0700

    text filter for expenses and remove button on each expense

--- src/app.js
+++ src/app.js
@@ -7,15 +7,10 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 store.dispatch(addExpense({ description: 'Water Bill', amount: 3000, createdAt: 200 }));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 2000, createdAt: 300 }));
-store.dispatch(setTextFilter('water'));
-const { expenses, filters } = store.getState();
-console.log(getVisibleExpenses(expenses, filters));
 
 const jsx = (
   <Provider store={store}>
