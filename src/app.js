c355d3c5b1653f365a5465ce93291d673f1aa38a
Pandurang Kamath <pandu1990@gmail.com>
2021-06-27 14:39:26 -0700

    Remove the dummy expenses being added at start of the app

--- src/app.js
+++ src/app.js
@@ -8,12 +8,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import { addExpense } from './actions/expenses';
-
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Water Bill', amount: 3000, createdAt: 200 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 2000, createdAt: 300 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1000, createdAt: 400 }));
 
 const jsx = (
   <Provider store={store}>
