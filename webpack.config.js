b40317737252b5979c8abef79175424868ea3048
Pandurang Kamath <pandu1990@gmail.com>
2021-12-19 23:31:10 -0800

    Update dependencies with vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,9 @@ module.exports = () => {
     },
     devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
     devServer: {
-      contentBase: path.join(__dirname, 'public'),
+      static: {
+        directory: path.join(__dirname, 'public')
+      },
       historyApiFallback: true
     }
   };
